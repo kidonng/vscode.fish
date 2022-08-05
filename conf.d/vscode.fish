b1b87925a51f79c33245588b9cc2e40125ce8bf5
Kid <hi@xuann.wang>
2022-08-05 16:20:10 +0800

    Trick from kitty shell integration

--- conf.d/vscode.fish
+++ conf.d/vscode.fish
@@ -1,28 +1,33 @@
-function __vsc_command_output_start -e fish_preexec
-    printf "\033]633;C\007"
-    printf "\033]633;E;%s\007" "$argv"
-end
+function __vsc_initialize -e fish_prompt
+    functions -e __vsc_initialize
+    test -n "$VSCODE_SHELL_INTEGRATION" && exit
+    set -g VSCODE_SHELL_INTEGRATION 1
 
-function __vsc_command_complete -e fish_postexec
-    printf "\033]633;D;%s\007" $status
-    __vsc_update_cwd
-end
+    function __vsc_command_output_start -e fish_preexec
+        printf "\033]633;C\007"
+        printf "\033]633;E;%s\007" "$argv"
+    end
 
-functions -q __vsc_fish_prompt && exit
-functions -c fish_prompt __vsc_fish_prompt
+    function __vsc_command_complete -e fish_postexec
+        printf "\033]633;D;%s\007" $status
+        __vsc_update_cwd
+    end
 
-function fish_prompt
-    __vsc_prompt_start
-    __vsc_fish_prompt
-    __vsc_prompt_end
-end
+    functions -c fish_prompt __vsc_fish_prompt
 
-if functions -q fish_right_prompt
-    functions -c fish_right_prompt __vsc_fish_right_prompt
+    function fish_prompt
+        __vsc_prompt_start
+        __vsc_fish_prompt
+        __vsc_prompt_end
+    end
 
-    function fish_right_prompt
-        __vsc_right_prompt_start
-        __vsc_fish_right_prompt
-        __vsc_right_prompt_end
+    if functions -q fish_right_prompt
+        functions -c fish_right_prompt __vsc_fish_right_prompt
+
+        function fish_right_prompt
+            __vsc_right_prompt_start
+            __vsc_fish_right_prompt
+            __vsc_right_prompt_end
+        end
     end
 end
